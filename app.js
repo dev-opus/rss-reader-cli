c3ca8681806da35ced45443f7477218c109e1291
Victor Orlunda <vicorlunda@gmail.com>
2021-04-27 02:01:22 +0100

    refactor: edit print method to handle undefined cases of authors

--- app.js
+++ app.js
@@ -37,9 +37,42 @@ const printFeedData = async data => {
   // if the data array is of length 2, it means we have a description field
   if (data.length == 2) {
     const [desc, items] = data;
-    console.log('description: ' + desc);
 
+    // check to see if the authors field is defined
+    const foundAuthors = items.find(item => item.author !== undefined);
+
+    /* 
+      this block runs if the authors field is not defined 
+      printing only the:
+        description
+        title and
+        link of each item in the rss-feed
+    */
+    if (!foundAuthors) {
+      console.log('\n\ndescription: ' + desc + '\n\n');
+      await sleep(1000);
+
+      for (let i = 0; i < items.length; i++) {
+        console.log(
+          `Title: ${items[i].title}\n\nLink: ${items[i].link}\n\n\n\n `
+        );
+        await sleep(1000);
+      }
+      return;
+    }
+
+    /*
+      this "block" runs if the authors filed is defined
+      printing the:
+        description
+        title
+        link and 
+        author of each item in the rss-feed
+    */
+
+    console.log('\n\ndescription: ' + desc + '\n\n\n');
     await sleep(1000);
+
     for (let i = 0; i < items.length; i++) {
       console.log(
         `Title: ${items[i].title}\n\nLink: ${items[i].link}\n\nAuthor: ${items[i].author} \n\n\n\n `
@@ -51,9 +84,37 @@ const printFeedData = async data => {
 
   // No description field
   const [items] = data;
+
+  // check to see if the authors field is defined
+  const foundAuthors = items.find(item => item.author !== undefined);
+
+  /* 
+      this block runs if the authors field is not defined 
+      printing only the:
+        title and
+        link of each item in the rss-feed
+    */
+  if (!foundAuthors) {
+    for (let i = 0; i < items.length; i++) {
+      console.log(
+        `Title: ${items[i].title}\n\nLink: ${items[i].link}\n\n\n\n `
+      );
+      await sleep(1000);
+    }
+    return;
+  }
+
+  /*
+      this "block" runs if the authors filed is defined
+      printing the:
+        title
+        link and 
+        author of each item in the rss-feed
+  */
+
   for (let i = 0; i < items.length; i++) {
     console.log(
-      `Title: ${items[i].title}\n\nLink: ${items[i].link}\n\nAuthor: ${items[i].author} \n\n\n `
+      `\n\nTitle: ${items[i].title}\n\nLink: ${items[i].link}\n\nAuthor: ${items[i].author} \n\n\n `
     );
     await sleep(1000);
   }
